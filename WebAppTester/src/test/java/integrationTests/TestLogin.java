293360a769d9637e3a0816fbbd628e89630a9c24
Ivan Tadic <tadic@mappi.helsinki.fi>
2013-05-20 08:40:57 +0300

    test5

--- WebAppTester/src/test/java/integrationTests/TestLogin.java
+++ WebAppTester/src/test/java/integrationTests/TestLogin.java
@@ -28,49 +28,47 @@ public class TestLogin {
     
     @Before
     public void setUp() {
+        // Open homepage
         driver = new HtmlUnitDriver();
         driver.get("https://www.dliv.in");
     }
-    
+    private void login(String username, String password){
+        // Login with given username and password
+        WebElement element= driver.findElement(By.name("email"));
+        element.sendKeys(username);
+        element = driver.findElement(By.name("pass"));
+        element.sendKeys(password);
+        element.submit();
+    }
     @Test
     public void userCanLoginWithCorrectUsernameAndPassword(){
-        String expected = "Ivan Tadic | Digital Living";
-        WebElement element= driver.findElement(By.name("email"));
-        element.sendKeys("ivan.tadic014@gmail.com");
-        element = driver.findElement(By.name("pass"));
-        element.sendKeys("roki014");
-        element.submit();
-        assertEquals(expected, driver.getTitle());
+        String expectedTitle = "Janne Laukkanen | Digital Living";
+        login("janne.laukkanen@digitalliving.fi","Mcnab2013");
+        assertEquals(expectedTitle, driver.getTitle());
     }
     @Test
     public void userCanNotLoginWithCorrectUsernameAndIncorrectPassword(){
-        String expected = "Digital Living";
-        WebElement element= driver.findElement(By.name("email"));
-        element.sendKeys("ivan.tadic014@gmail.com");
-        element = driver.findElement(By.name("pass"));
-        element.sendKeys("somePassword");
-        element.submit();
-        assertEquals(expected, driver.getTitle());
+        String expectedTitle = "Digital Living";
+        login("ivan.tadic014@gmail.com","somepassword");
+        assertEquals(expectedTitle, driver.getTitle());
     }
     @Test
     public void userCanNotLoginWithIncorrectUsernameAndSomePassword(){
-        String expected = "Digital Living";
-        WebElement element= driver.findElement(By.name("email"));
-        element.sendKeys("ivan.tadic014@gm");
-        element = driver.findElement(By.name("pass"));
-        element.sendKeys("somePassword");
-        element.submit();
-        assertEquals(expected, driver.getTitle());
+        String expectedTitle = "Digital Living";
+        login("janne.laukkanen@digitallivin.com","asdasdasd");
+        assertEquals(expectedTitle, driver.getTitle());
+    }
+    @Test
+    public void userCanNotLoginWithUsernameAndEmptyPassword(){
+        String expectedTitle = "Digital Living";
+        login("janne.laukkanen@digitalliving.fi","");
+        assertEquals(expectedTitle, driver.getTitle());
     }
     @Test
     public void userCanNotLoginWithEmptyUsernameAndPassword(){
-        String expected = "Digital Living";
-        WebElement element= driver.findElement(By.name("email"));
-        element.sendKeys("");
-        element = driver.findElement(By.name("pass"));
-        element.sendKeys("");
-        element.submit();
-        assertEquals(expected, driver.getTitle());
+        String expectedTitle = "Digital Living";
+        login("","");
+        assertEquals(expectedTitle, driver.getTitle());
     }
     
 
